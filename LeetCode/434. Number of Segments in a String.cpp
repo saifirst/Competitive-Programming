4400e418b946e6cb7c5c4d70ede6d7830311f56e
Saif Btoush <saiif.btoush@gmail.com>
2024-03-03 20:29:08 +0300

    New solution added

---
+++ LeetCode/434. Number of Segments in a String.cpp
@@ -0,0 +1,21 @@
+/*
+  ____        _  __   ____  _                  _     
+ / ___|  __ _(_)/ _| | __ )| |_ ___  _   _ ___| |__  
+ \___ \ / _` | | |_  |  _ \| __/ _ \| | | / __| '_ \ 
+  ___) | (_| | |  _| | |_) | || (_) | |_| \__ \ | | |
+ |____/ \__,_|_|_|   |____/ \__\___/ \__,_|___/_| |_|                                                   
+                                                                                  
+https://www.linkedin.com/in/saif-btoush/                                                                                                                                                                              
+*/
+class Solution {
+public:
+    int countSegments(string s) {
+    stringstream ss(s);
+    string word;
+    int count = 0;
+    while (ss >> word) {
+        count++;
+    }
+    return count;
+    }
+};
