210ac627b735c6550b441a6337bec7e16232b258
Saif Btoush <saiif.btoush@gmail.com>
2024-03-10 11:40:43 +0300

    New solution added

---
+++ LeetCode/535. Encode and Decode TinyURL.cpp
@@ -0,0 +1,18 @@
+/*
+  ____        _  __   ____  _                  _     
+ / ___|  __ _(_)/ _| | __ )| |_ ___  _   _ ___| |__  
+ \___ \ / _` | | |_  |  _ \| __/ _ \| | | / __| '_ \ 
+  ___) | (_| | |  _| | |_) | || (_) | |_| \__ \ | | |
+ |____/ \__,_|_|_|   |____/ \__\___/ \__,_|___/_| |_|                                                   
+                                                                                  
+https://www.linkedin.com/in/saif-btoush/                                                                                                                                                                              
+*/
+class Solution {
+public:
+    string encode(string longUrl) {
+        return longUrl;
+    }
+    string decode(string shortUrl) {
+        return shortUrl;
+    }
+};
