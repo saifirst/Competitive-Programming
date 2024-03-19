002a7c7189ce9840aa9fd61b208b8b9a92c03523
Saif Btoush <saiif.btoush@gmail.com>
2024-03-19 15:49:00 +0300

    New solution added

---
+++ LeetCode/2335. Minimum Amount of Time to Fill Cups.cpp
@@ -0,0 +1,18 @@
+/*
+  ____        _  __   ____  _                  _     
+ / ___|  __ _(_)/ _| | __ )| |_ ___  _   _ ___| |__  
+ \___ \ / _` | | |_  |  _ \| __/ _ \| | | / __| '_ \ 
+  ___) | (_| | |  _| | |_) | || (_) | |_| \__ \ | | |
+ |____/ \__,_|_|_|   |____/ \__\___/ \__,_|___/_| |_|                                                   
+                                                                                  
+https://www.linkedin.com/in/saif-btoush/                                                                                                                                                                              
+*/
+class Solution {
+public:
+    int fillCups(vector<int>& amount) {
+        sort(amount.begin(),amount.end());
+        if(amount[2]>=amount[0]+amount[1])return amount[2];
+        int sum=amount[0]+amount[1]+amount[2];
+        return sum/2+sum%2;
+    }
+};
