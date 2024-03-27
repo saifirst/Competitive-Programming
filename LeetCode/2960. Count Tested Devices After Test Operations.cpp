e58724b7d98b21f78a6824e3eccb315ae9898f51
Saif Btoush <saiif.btoush@gmail.com>
2024-03-27 23:35:43 +0300

    New solution added

---
+++ LeetCode/2960. Count Tested Devices After Test Operations.cpp
@@ -0,0 +1,19 @@
+/*
+  ____        _  __   ____  _                  _     
+ / ___|  __ _(_)/ _| | __ )| |_ ___  _   _ ___| |__  
+ \___ \ / _` | | |_  |  _ \| __/ _ \| | | / __| '_ \ 
+  ___) | (_| | |  _| | |_) | || (_) | |_| \__ \ | | |
+ |____/ \__,_|_|_|   |____/ \__\___/ \__,_|___/_| |_|                                                   
+                                                                                  
+https://www.linkedin.com/in/saif-btoush/                                                                                                                                                                              
+*/
+class Solution {
+public:
+    int countTestedDevices(vector<int>& batteryPercentages) {
+      int res=0;
+      for(int i=0;i<batteryPercentages.size();i++){
+          if(batteryPercentages[i]-res>0)res++;
+      }
+      return res;
+    }
+};
