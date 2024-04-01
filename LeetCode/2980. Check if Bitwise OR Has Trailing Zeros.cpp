e842eebe40e653185bde1be76cf923587cd3ef0b
Saif Btoush <saiif.btoush@gmail.com>
2024-04-01 12:03:03 +0300

    New solution added

---
+++ LeetCode/2980. Check if Bitwise OR Has Trailing Zeros.cpp
@@ -0,0 +1,20 @@
+/*
+  ____        _  __   ____  _                  _     
+ / ___|  __ _(_)/ _| | __ )| |_ ___  _   _ ___| |__  
+ \___ \ / _` | | |_  |  _ \| __/ _ \| | | / __| '_ \ 
+  ___) | (_| | |  _| | |_) | || (_) | |_| \__ \ | | |
+ |____/ \__,_|_|_|   |____/ \__\___/ \__,_|___/_| |_|                                                   
+                                                                                  
+https://www.linkedin.com/in/saif-btoush/                                                                                                                                                                              
+*/
+class Solution {
+public:
+    bool hasTrailingZeros(vector<int>& nums) {
+        int evens=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==0)evens++;
+            if(evens>=2)return true;
+        }
+        return false;
+    }
+};
