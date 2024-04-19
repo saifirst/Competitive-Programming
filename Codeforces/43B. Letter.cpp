d83070654f4413d113ce231c309f01b4a775abb2
Saif Btoush <saiif.btoush@gmail.com>
2024-04-20 02:03:58 +0300

    New solution added

---
+++ Codeforces/43B. Letter.cpp
@@ -0,0 +1,44 @@
+/*
+  ____        _  __   ____  _                  _     
+ / ___|  __ _(_)/ _| | __ )| |_ ___  _   _ ___| |__  
+ \___ \ / _` | | |_  |  _ \| __/ _ \| | | / __| '_ \ 
+  ___) | (_| | |  _| | |_) | || (_) | |_| \__ \ | | |
+ |____/ \__,_|_|_|   |____/ \__\___/ \__,_|___/_| |_|                                                   
+                                                                                  
+https://www.linkedin.com/in/saif-btoush/                                                                                                                                                                              
+*/
+using namespace std;
+#include <iostream>
+#include<bits/stdc++.h>
+#include <cmath>
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include<vector>
+#define fastread() (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+#define yes cout<<"YES"<<endl;
+#define sf scanf
+#define no cout<<"NO"<<endl;
+#define ll long long
+#define input(arr); for(int i=0;i<n;i++)cin>>arr[i];
+#define output(arr); for(int i=0;i<n;i++)cout<<arr[i]<<" ";
+#define setoutput(set); for(auto i=set.begin();i!=set.end();i++)cout<<*i<<" ";
+#define FIO ios_base::sync_with_stdio(0);cin.tie(0);
+#define sortvec(ve); sort(ve.begin(),ve.end());
+#define sortarr(arr); sort(arr,arr+n);
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+
+string s1,s2;getline(cin,s1);getline(cin,s2);
+for(int i=0;i<s2.length();i++){
+    if(s2[i]!=' '){
+        if(count(s2.begin(),s2.end(),s2[i])>count(s1.begin(),s1.end(),s2[i])){
+            no
+            return 0;
+        }
+    }
+}
+    yes
+
+}
