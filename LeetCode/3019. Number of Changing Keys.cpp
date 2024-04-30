b5e920c0634d281c1f7d655d0af824c238e0181a
Saif Btoush <saiif.btoush@gmail.com>
2024-04-30 12:18:20 +0300

    New solution added

---
+++ LeetCode/3019. Number of Changing Keys.cpp
@@ -0,0 +1,19 @@
+/*
+  ____        _  __   ____  _                  _     
+ / ___|  __ _(_)/ _| | __ )| |_ ___  _   _ ___| |__  
+ \___ \ / _` | | |_  |  _ \| __/ _ \| | | / __| '_ \ 
+  ___) | (_| | |  _| | |_) | || (_) | |_| \__ \ | | |
+ |____/ \__,_|_|_|   |____/ \__\___/ \__,_|___/_| |_|                                                   
+                                                                                  
+https://www.linkedin.com/in/saif-btoush/                                                                                                                                                                              
+*/
+class Solution {
+public:
+    int countKeyChanges(string s) {
+        int res=0;
+        for(int i=1;i<s.length();i++){
+            if(tolower(s[i])!=tolower(s[i-1]))res++;
+        }
+        return res;
+    }
+};
