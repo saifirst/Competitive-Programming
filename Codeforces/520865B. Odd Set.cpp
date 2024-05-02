cd841c2aab7656e9b3ecd8b666406385b98ce59a
Saif Btoush <saiif.btoush@gmail.com>
2024-05-02 21:46:22 +0300

    New solution added

---
+++ Codeforces/520865B. Odd Set.cpp
@@ -0,0 +1,47 @@
+/*
+  ____        _  __   ____  _                  _     
+ / ___|  __ _(_)/ _| | __ )| |_ ___  _   _ ___| |__  
+ \___ \ / _` | | |_  |  _ \| __/ _ \| | | / __| '_ \ 
+  ___) | (_| | |  _| | |_) | || (_) | |_| \__ \ | | |
+ |____/ \__,_|_|_|   |____/ \__\___/ \__,_|___/_| |_|                                                   
+                                                                                  
+https://www.linkedin.com/in/saif-btoush/                                                                                                                                                                              
+*/
+using namespace std;
+#include <iostream>
+#include<bits/stdc++.h>
+#include <cmath>
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include<vector>
+#define fastread() (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+#define yes cout<<"YES"<<endl;
+#define sf scanf
+#define no cout<<"NO"<<endl;
+#define ll long long
+#define input(arr); for(int i=0;i<n;i++)cin>>arr[i];
+#define output(arr); for(int i=0;i<n;i++)cout<<arr[i]<<" ";
+#define setoutput(set); for(auto i=set.begin();i!=set.end();i++)cout<<*i<<" ";
+#define FIO ios_base::sync_with_stdio(0);cin.tie(0);
+#define sortvec(ve); sort(ve.begin(),ve.end());
+#define sortarr(arr); sort(arr,arr+n);
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+ 
+int t;cin>>t;
+while(t--){
+    int n;cin>>n;
+    int arr[2*n];
+    int odds=0;
+    for(int i=0;i<2*n;i++){
+        cin>>arr[i];
+        if(arr[i]%2!=0)odds++;
+    }
+    if(odds==n)cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+ 
+}
+ 
+}
