e98e66a3934e43cd66e25ec375fca195dd150495
Saif Btoush <saiif.btoush@gmail.com>
2024-05-13 21:48:15 +0300

    New solution added

---
+++ LeetCode/3046. Split the Array.cpp
@@ -0,0 +1,18 @@
+/*
+  ____        _  __   ____  _                  _     
+ / ___|  __ _(_)/ _| | __ )| |_ ___  _   _ ___| |__  
+ \___ \ / _` | | |_  |  _ \| __/ _ \| | | / __| '_ \ 
+  ___) | (_| | |  _| | |_) | || (_) | |_| \__ \ | | |
+ |____/ \__,_|_|_|   |____/ \__\___/ \__,_|___/_| |_|                                                   
+                                                                                  
+https://www.linkedin.com/in/saif-btoush/                                                                                                                                                                              
+*/
+class Solution {
+public:
+    bool isPossibleToSplit(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++){
+            if(count(nums.begin(),nums.end(),nums[i])>2)return false;
+        }
+        return true;
+    }
+};
