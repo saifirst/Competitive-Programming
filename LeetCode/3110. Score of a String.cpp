a9c0b32dc4a6795dc4d3928d87173635bebf7044
Saif Btoush <saiif.btoush@gmail.com>
2024-05-16 01:23:18 +0300

    New solution added

---
+++ LeetCode/3110. Score of a String.cpp
@@ -0,0 +1,19 @@
+/*
+  ____        _  __   ____  _                  _     
+ / ___|  __ _(_)/ _| | __ )| |_ ___  _   _ ___| |__  
+ \___ \ / _` | | |_  |  _ \| __/ _ \| | | / __| '_ \ 
+  ___) | (_| | |  _| | |_) | || (_) | |_| \__ \ | | |
+ |____/ \__,_|_|_|   |____/ \__\___/ \__,_|___/_| |_|                                                   
+                                                                                  
+https://www.linkedin.com/in/saif-btoush/                                                                                                                                                                              
+*/
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int res=0;
+        for(int i=0;i<s.length()-1;i++){
+            res+=abs((s[i]-'0')-(s[i+1]-'0'));
+        }
+        return res;
+    }
+};
