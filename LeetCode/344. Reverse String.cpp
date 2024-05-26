b190e5b70e734bff12fcf41c4e4ff0fbe4154a5f
Saif Btoush <saiif.btoush@gmail.com>
2024-05-26 14:25:23 +0300

    New solution added

---
+++ LeetCode/344. Reverse String.cpp
@@ -0,0 +1,15 @@
+/*
+  ____        _  __   ____  _                  _     
+ / ___|  __ _(_)/ _| | __ )| |_ ___  _   _ ___| |__  
+ \___ \ / _` | | |_  |  _ \| __/ _ \| | | / __| '_ \ 
+  ___) | (_| | |  _| | |_) | || (_) | |_| \__ \ | | |
+ |____/ \__,_|_|_|   |____/ \__\___/ \__,_|___/_| |_|                                                   
+                                                                                  
+https://www.linkedin.com/in/saif-btoush/                                                                                                                                                                              
+*/
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        reverse(s.begin(),s.end());
+    }
+};
