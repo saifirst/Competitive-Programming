1c8c906023100c4f10cdbd43745b3b1f36b1394d
Saif Btoush <saiif.btoush@gmail.com>
2024-06-22 13:37:00 +0300

    New solution added

---
+++ LeetCode/3131. Find the Integer Added to Array I.cpp
@@ -0,0 +1,18 @@
+/*
+  ____        _  __   ____  _                  _     
+ / ___|  __ _(_)/ _| | __ )| |_ ___  _   _ ___| |__  
+ \___ \ / _` | | |_  |  _ \| __/ _ \| | | / __| '_ \ 
+  ___) | (_| | |  _| | |_) | || (_) | |_| \__ \ | | |
+ |____/ \__,_|_|_|   |____/ \__\___/ \__,_|___/_| |_|                                                   
+                                                                                  
+https://www.linkedin.com/in/saif-btoush/                                                                                                                                                                              
+*/
+class Solution {
+public:
+    int addedInteger(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+
+        return nums2[0]-nums1[0];
+    }
+};
