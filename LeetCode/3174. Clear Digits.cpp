2c839add0d57a7627edc2d5167b13d80f82aecda
Saif Btoush <saiif.btoush@gmail.com>
2024-07-17 13:10:03 +0300

    New solution added

---
+++ LeetCode/3174. Clear Digits.cpp
@@ -0,0 +1,26 @@
+/*
+  ____        _  __   ____  _                  _     
+ / ___|  __ _(_)/ _| | __ )| |_ ___  _   _ ___| |__  
+ \___ \ / _` | | |_  |  _ \| __/ _ \| | | / __| '_ \ 
+  ___) | (_| | |  _| | |_) | || (_) | |_| \__ \ | | |
+ |____/ \__,_|_|_|   |____/ \__\___/ \__,_|___/_| |_|                                                   
+                                                                                  
+https://www.linkedin.com/in/saif-btoush/                                                                                                                                                                              
+*/
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char>st;
+        string res="";
+        for(int i=0;i<s.length();i++){
+            if(!st.empty()&&isalpha(st.top())&&isdigit(s[i]))st.pop();
+            else st.push(s[i]);
+        }
+        while(!st.empty()){
+            res+=st.top();
+            st.pop();
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
